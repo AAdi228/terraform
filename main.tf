ebfa410e8dce4bc51460fa1a306e90905e8e4ae8
AAdi228 <162331634+AAdi228@users.noreply.github.com>
2024-04-03 19:16:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "Adithya" {
   ami = "ami-05295b6e6c790593e"
   instance_type = "t2.micro"
-  key_name = "ggt"   
+  key_name = "adithya"   
   tags = {
     Name = "Adithya"
   }
